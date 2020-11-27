884002642d75a249393ef3b23006ae0b98ec6240
FLacombe <francois.lacombe@infos-reseaux.com>
2020-11-27 22:53:24 +0100

    Fix

--- src/geocoder/addok.js
+++ src/geocoder/addok.js
@@ -4,21 +4,20 @@ class AddokGeocoder {
     constructor(host) {
         this._host = host;
     }
-
-    _geocode = async function (query){
-        // Requête HTTP
-        let addokResp = await axios.get(_host+"/search/?q="+query+"&limit=10");
-
-        // Retour des résultats attendus par Mapbox
-        let result = [];
-        if (addokResp != null){
-            result = addokResp.features;
-        }
-
-        return result;
-    }
-
+    
     static geocode (query){
+        let _geocode = async function (query){
+            // Requête HTTP
+            let addokResp = await axios.get(_host+"/search/?q="+query+"&limit=10");
+    
+            // Retour des résultats attendus par Mapbox
+            let result = [];
+            if (addokResp != null){
+                result = addokResp.features;
+            }
+    
+            return result;
+        }
         return _geocode(query);
     }
 }
