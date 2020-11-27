4f07c31da097e929a7e62db82ca1ef74e61489a8
FLacombe <francois.lacombe@infos-reseaux.com>
2020-11-27 22:49:49 +0100

    Fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const {merge} = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
